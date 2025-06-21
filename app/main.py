b32acb1c5cf3f0b22df993722670d714bd5aa52e
ankitnarang <ankit2@relinns.com>
2025-06-21 13:24:16 +0530

    [Updated] Cors origin to allow all

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 #cors to allow requests from frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
